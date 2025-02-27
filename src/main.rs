8f598029e55618666dfd41797f3cf6924c8c104e
Lily <joshrl@proton.me>
2025-02-27 20:19:13 +0000

    ready for publish

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use include_folder::Directory;
-use include_folder::include_folder;
+// use include_folder::Directory;
+// use include_folder::include_folder;
 
-include_folder!("./include_folder_macros/test_folder", "TestDir");
+// include_folder!("./include_folder_macros/test_folder", "TestDir");
 
 fn main() {
-    let dir = test_dir();
-    dbg!(dir.files());
+    // let dir = test_dir();
+    // dbg!(dir.files());
 }
